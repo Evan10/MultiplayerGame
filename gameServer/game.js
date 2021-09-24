28d623ce23f768196092a7e048c233f313f90480
Evan Royal <evanroyal1@gmail.com>
2021-09-24 19:33:13 -0400

    fixed bug that made new players not see king

--- gameServer/game.js
+++ gameServer/game.js
@@ -152,6 +152,7 @@ module.exports = class GameInstance {
       console.log(playerinfo);
       socket.emit("new_player", playerinfo);
     }
+    socket.emit("new-king",playerWithMostKills.ID);
     let x = Math.random() * this.gameMapSize.w,
       y = Math.random() * this.gameMapSize.h;
     this.players.push(new Player(id, x, y, socket, this, playerName));
